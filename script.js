d498044c12a1ebdbc4bb0891e93dc421fddad3b5
juug24btech14820-hash <juug24btech14820@jainuniversity.ac.in>
2026-04-23 13:50:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
-function appendValue(value) {
-    document.getElementById("display").value += value;
-}
+window.onload = function () {
+    loadTasks();
+    updateCount();
+};
 
-function clearDisplay() {
-    document.getElementById("display").value = "";
-}
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let message = document.getElementById("message");
+    let task = input.value.trim();
 
-function calculate() {
-    let display = document.getElementById("display");
-
-    try {
-        display.value = eval(display.value);
-    } catch {
-        display.value = "Error";
+    if (task === "") {
+        message.innerText = "Command required!";
+        return;
     }
+
+    message.innerText = "";
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+    <span>> ${task}</span>
+    <div>
+        <button onclick='completeTask(this)'>✔</button>
+        <button onclick='deleteTask(this)'>✖</button>
+    </div>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+
+    saveTasks();
+    updateCount();
+    input.value = "";
+}
+
+function completeTask(btn) {
+    btn.parentElement.parentElement.classList.toggle("completed");
+    saveTasks();
+}
+
+function deleteTask(btn) {
+    btn.parentElement.parentElement.remove();
+    saveTasks();
+    updateCount();
+}
+
+function clearAll() {
+    document.getElementById("taskList").innerHTML = "";
+    localStorage.removeItem("tasks");
+    updateCount();
+}
+
+function saveTasks() {
+    let tasks = document.getElementById("taskList").innerHTML;
+    localStorage.setItem("tasks", tasks);
+}
+
+function loadTasks() {
+    let saved = localStorage.getItem("tasks");
+    if (saved) {
+        document.getElementById("taskList").innerHTML = saved;
+    }
+}
+
+function updateCount() {
+    let count = document.getElementById("taskList").children.length;
+    document.getElementById("count").innerText = count + " Logs";
 }
